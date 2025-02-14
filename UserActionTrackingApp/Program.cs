38cf60049e6b7265fb78f4655a89eb7f1293105d
RudraPatelhere <Rpatel1198@conestogac.on.ca>
2025-02-14 13:45:15 -0500

    Initial Code

--- UserActionTrackingApp/Program.cs
+++ UserActionTrackingApp/Program.cs
@@ -1,27 +1,34 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// ADD SESSION SUPPORT
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(5); // SO, THIS WILL SET SESSION TIMEOUT
+    options.Cookie.HttpOnly = true; // THIS WILL MAKE COOKIE MORE SECURE
+    options.Cookie.IsEssential = true; // THIS WILL ENSURE COOKIE ALWAYS WORKS
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
+
+app.UseSession(); // ENABLE SESSION MIDDLEWARE
 
 app.UseAuthorization();
 
 app.MapControllerRoute(
-    name: "default",
+    name: "DEFAULT_ROUTE", // SO, THIS WILL BE THE DEFAULT ROUTE
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
